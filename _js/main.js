425f1ca0d377be88a51db47ab78584bafb2fbb49
Danilo <daniloadscavalcante@gmail.com>
2023-03-27 18:50:32 -0300

    inclusão do lightbox e dados do formulario

--- _js/main.js
+++ _js/main.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 const closeToggle = document.querySelector(".close-toggle");
 
-toggle.addEventListener("click", ()=>{
+toggle.addEventListener("click", () =>{
     menu.style.display="flex";
 });
 
@@ -10,20 +10,52 @@ closeToggle.addEventListener("click", ()=>{
     menu.style.display="none";
 });
 
-function sendMail(){
-    var dados = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        whatsapp: document.getElementById("zap").value
-    };
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+
+const imagens = document.querySelectorAll(".galeria img");
+imagens.forEach(imagem =>{
+    imagem.addEventListener("click", e => {
+        lightbox.classList.add("active");
+        const img = document.createElement("img");
+        img.src = imagem.src;  
+        img.style.width="auto";
+        img.style.height="400px";
+        while(lightbox.firstChild){
+            lightbox.removeChild(lightbox.firstChild);
+        }             
+        lightbox.appendChild(img);
+    })
+})
+
+lightbox.addEventListener("click", e => {
+    if(e.target !== e.currentTarget) return
+    lightbox.classList.remove("active")
+})
+const btneviar = document.querySelector("#enviar");
+function send(){
+   
+        emailjs.send("service_afeo8ha","template_ysikz9b",{
+            name: document.querySelector("#form input[type=text]").value,
+            email: document.querySelector("#form input[type=email]").value,
+            whatsapp: document.querySelector("#form input[type=tel]").value,          
+        }).then(response=>{
+            console.log("sucesso", response.status);
+        },(error)=>{
+            console.log(error);
+        })
 }
 
 
+btneviar.addEventListener("click", e => {
+    e.preventDefault()    
+    send()
+    document.querySelector('#form').reset();
+})
 
 
-emailjs.send(serviceID,templateID,dados)
-.then(function (res) {
-        
-        alert("seu email envou" + res.status);
-    })
-   
+
+
+
+
